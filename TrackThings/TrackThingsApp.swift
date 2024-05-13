fdd390f354f8bbb72ef770003187d2f9d2eb4ba7
Ben Howlett <ben@benhowlett.com>
2024-05-13 09:16:33 -0600

    Initial Commit

---
+++ TrackThings/TrackThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackThingsApp.swift
+//  TrackThings
+//
+//  Created by Ben Howlett on 2024-05-13.
+//
+
+import SwiftUI
+
+@main
+struct TrackThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
